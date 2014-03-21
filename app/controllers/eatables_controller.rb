a00a5ef97ff80fd04be68e6c6adfec460162ae2f
davedawson <dave@iamdavedawson.com>
2014-03-21 16:13:05 -0400

    [Routes, Eatables] Set up routes, create index for Eatables

--- app/controllers/eatables_controller.rb
+++ app/controllers/eatables_controller.rb
@@ -1,3 +1,5 @@
 class EatablesController < ApplicationController
-
+  def index
+    @eatables = Eatable.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Whatsinseason::Application.routes.draw do
+  resources :eatables
+  resources :foods
+  resources :locations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
