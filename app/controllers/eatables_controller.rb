e4da316cb4d76a9f6d5af280b06e56f93640cd57
davedawson <dave@iamdavedawson.com>
2014-03-23 23:31:25 -0400

    [Eatable Controller] filter by today's date

--- app/controllers/eatables_controller.rb
+++ app/controllers/eatables_controller.rb
@@ -18,10 +18,22 @@ class EatablesController < ApplicationController
   end
 
   def index
-    # @eatables = Eatable.all
-    # @eatables = Eatable.joins(:food).where(:eatables => { :fight_id => @fight.id })
     @eatables = Eatable.all
+    @date = Date.today.month
+    @month = @date
+    # @eatables.each do |eatable|
+    #   if eatable.month_ids.include?(2)
+    #     puts @eatable
+    #   end
+    # end
+
+    # @eatables = Eatable.joins(:food).where(:eatables => { :fight_id => @fight.id })
+    # @eatables = Eatable.where(:month_ids => 2)
+
+    # @passport_visa_supplement_o = PassportVisa.includes([:assets, :country]).uniq.where(:visa_type => 'Official' ,:assets => {:pdf_type => 'supplement'}).order("countries.name ASC")
+
   end
+
   def show
     @eatable = Eatable.find params[:id]
     @months = @eatable.months
