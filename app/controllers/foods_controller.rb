75ff7801140ca80716c2b2e668de773023f99d8d
davedawson <dave@iamdavedawson.com>
2014-03-24 13:37:40 -0400

    [Foods Controller] Foods now submit properly

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,18 +2,33 @@ class FoodsController < ApplicationController
   def index
     @foods = Food.all
   end
+
   def new
     @food = Food.new
     @locations = Location.all
   end
+
+  def show
+    @food = Food.find params[:id]
+  end
   def create
-    @food = Food.new(params[:food])
+    @food = Food.create!(food_params)
     # @food.locations << Location.find_by_id(params[:months])
     # @food.attributes = {'locations_ids' => []}.merge(params[:food] || {})
     if @food.save
-        redirect_to foods_url(@food), :notice => "Food created!"
+        redirect_to food_path(@food), :notice => "Food created!"
     else
       render :action => "new"
     end
   end
+
+  private
+    def food_params
+      params.require(:food).permit(
+        :id,
+        :title,
+        :description,
+        :image
+      )
+    end
 end
